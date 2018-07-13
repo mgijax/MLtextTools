3f86a899f152e35d4e2d6d578facf58dc7b2c073
Jim Kadin <james.kadin@jax.org>
2018-07-13 12:10:19 -0400

    refSection.py major restructure. Added base class, new ref section prediction algorthm (look for Refs, Lit Cited before other words...)

--- RefSection/refSectionLib.py
+++ RefSection/refSectionLib.py
@@ -1,34 +1,21 @@
 #!/usr/bin/env python2.7
 
+"""
+Classes for predicting the location of reference sections in text from
+scientific articles
+"""
+
 import string
 import re
 
-refKeyTerms = [ "References",
-		"Reference",
-		"Literature Cited",
-		"Acknowledgements",
-		"Acknowledgments",
-		"Conflicts of Interest",
-		"Conflict of Interest",
-		]
-
-class RefSectionRemover (object):
+class BaseRefSectionRemover (object):
     '''
-    Class to predict/remove Reference sections from (extracted) text strings
+    Abstract base class for reference section predictor/removal classes
     '''
-
-    def __init__(self,
-		 keyTerms=refKeyTerms,	# terms that can begin a ref section
-		 maxFraction=0.4	# max fraction of whole doc that the
-		 			#  ref section is allowed to be
-		):
-	self.keyTerms = keyTerms
-	self.maxFraction = maxFraction
-	self.regexString = self.getRegexString()
-	self.refRegex = re.compile(self.regexString)
-    # -----------------------
-
     def removeRefSection(self, text):
+	"""
+	return text with the predicted reference section removed
+	"""
 
 	kw, refStart = self.predictRefSection(text)
 	return text[:refStart]
@@ -37,57 +24,221 @@ class RefSectionRemover (object):
     # ----------------------------------
 
     def getRefSection(self, text):
+	"""
+	Return the predicted reference section (string)
+	"""
 
 	kw, refStart = self.predictRefSection(text)
 	return text[refStart:]
     # ----------------------------------
 
     def predictRefSection(self, text):
-	# predict ref section.
-	# Return the key term matched and the position of the predicted
-	#  refs section in text.
-
-	textLength = len(text)
-	matches = [ x for x in self.refRegex.finditer(text)]
-
-	if len(matches) == 0:
-	    lastKeyWord = "No match"
-	    refStart = textLength
-	else:
-	    m = matches[-1]
-	    lastKeyWord = m.group(1)	# last matched term
-	    refStart = m.start(1)	# position at end of that term
-	    refLength = textLength - refStart
-
-	    if float(refLength)/textLength > self.maxFraction:
-		lastKeyWord = "Ref Prediction > %4.2f" % self.maxFraction
-		refStart = textLength
-
-	return lastKeyWord, refStart
+	"""
+	Abstract method.
+	Predict ref section.
+	Return the text found that we presume starts of the reference
+	    section and the position of it in full text.
+	"""
+	pass
+	return "splitKeyWord", len(text)
     # ----------------------------------
 
-    def getRegexString(self):
+    def buildRegexString(self, keyTerms):
+	"""
 	# for the set of keyTerms, return regex pattern string that matches
 	# \n(term1|term2|term3)\n - with each term "spacesAndCaseInsensitive"
 	# Assuming the doc still has newline's the '\n's match only if the
 	#   keyTerm is on a line by itself - which works well for section
 	#   headings.
 	# If we apply this to docs where newline's have been removed, we'd
-	#   to replace '\n's with '\b's to match work boundaries.
-	#   (could add this as a param to constructor at some point)
-	# The "spacesAndCaseInsensitive" comes from:
+	#   have to replace '\n's with '\b's to match work boundaries.
+	#   (could add this as an option at some point)
+	# Why "spacesAndCaseInsensitive"?
 	#   Diff journals use different case conventions, and the text
 	#   extraction algorithm sometimes converts case AND sometimes adds
 	#   spaces between the individual letters (no idea why)
-
+	"""
 	regexStr = r'\n('
-	insensitive = map(spacesAndCaseInsensitiveRegex, self.keyTerms)
+	insensitive = map(spacesAndCaseInsensitiveRegex, keyTerms)
 	regexStr += '|'.join(insensitive)
 	regexStr += r')\n'
 	return regexStr
     # ----------------------------------
 
-#------------------ end Class RefSectionRemover
+    def isReasonableRefStart(self, refStart, totalTextLength):
+	"""
+	Is the predicted reference start location ok?
+	(or is it too early in the document so too much of the doc would be
+	 thrown away?)
+	Assumes subclass has self.maxFraction
+	"""
+	refLength = totalTextLength - refStart
+
+	return float(refLength)/totalTextLength <= self.maxFraction
+    # ----------------------------------
+
+    def getRegexString(self):
+	"""
+	Assumes subclass has self.regexString
+	"""
+	return self.regexString
+    # ----------------------------------
+
+#------------------ end Class BaseRefSectionRemover
+
+class RefSectionRemover (BaseRefSectionRemover):
+    '''
+    Class to predict/remove Reference sections from (extracted) text strings.
+    Approach:
+    Looks 1st for primary key terms, and if found uses their earliest location.
+    If no primary key terms are found, then look for other potential terms, 
+    use earliest location.
+    '''
+    # Dict of key terms and whether they are a primary term
+    keyTermDict = {
+		"References":            {"isPrimary":True},
+		"Reference":             {"isPrimary":True},
+		"Literature Cited":      {"isPrimary":True},
+		"Acknowledgements":      {"isPrimary":False},
+		"Acknowledgments":       {"isPrimary":False},
+		"Conflicts of Interest": {"isPrimary":False},
+		"Conflict of Interest":  {"isPrimary":False},
+		}
+    # dict of squashed key terms (no spaces, lower case)
+    squashedKeyTerms = \
+	dict( [ [x.replace(" ","").lower(), x] for x in keyTermDict.keys() ] )
+
+    def __init__(self,
+		 maxFraction=0.4	# max fraction of whole doc that the
+		 			#  ref section is allowed to be
+		):
+	self.keyTerms = self.keyTermDict.keys()
+	self.maxFraction = maxFraction
+	self.regexString = self.buildRegexString(self.keyTerms)
+	self.refRegex = re.compile(self.regexString)
+    # -----------------------
+
+    def predictRefSection(self, text):
+	"""
+	Predict ref section.
+	Return the text found that we presume starts of the reference
+	    section and the position of it in full text.
+	"""
+	textLength = len(text)
+
+	# match the reg exp and get list of re.match objects (in order matched)
+	reMatches = [ x for x in self.refRegex.finditer(text)]
+
+	if len(reMatches) == 0:
+	    return "No match", textLength
+
+	primary, secondary = self.organizeMatches(reMatches)
+
+	for m in primary:
+	    if self.isReasonableRefStart(m['termStart'], textLength):
+		return m['matchedText'], m['termStart']
+
+	for m in secondary:
+	    if self.isReasonableRefStart(m['termStart'], textLength):
+		return m['matchedText'], m['termStart']
+
+	# no match with reasonable location found
+	reason = "Ref Prediction > %4.2f" % self.maxFraction
+	return reason, textLength
+    # ----------------------------------
+
+    def organizeMatches(self, reMatches):
+	"""
+	Return two lists: "match descriptions" to primary terms and to
+	    secondary terms.
+	Each "match description" is
+	    {'termStart': n, 'matchedText': str, 'keyTerm': str}
+	"""
+	primary = []
+	secondary = []
+
+	for reM in reMatches:
+
+	    # assume there is only one group (w/ the matching text) per reMatch
+	    matchedText = reM.group(1)
+	    keyTerm = self.getMatchedKeyTerm(matchedText)
+	    matchDesc = {'matchedText': matchedText,
+			'keyTerm':     keyTerm,
+			'termStart':   reM.start(1),
+			}
+	    if self.keyTermDict[keyTerm]['isPrimary']:
+		primary.append(matchDesc)
+	    else:
+		secondary.append(matchDesc)
+
+	return primary, secondary
+    # ----------------------------------
+
+    def getMatchedKeyTerm(self, matchedText):
+	"""
+	Convert matchedText to an actual key term.
+	(remove spaces, lower case)
+	This effectively "undoes" the weirdness we do to create the regular
+	expression for matching the key terms
+	"""
+	cleansedTerm = matchedText.replace(" ","").lower()
+	return self.squashedKeyTerms[cleansedTerm]
+    # ----------------------------------
+
+#------------------ end Class RefSectionRemover_new
+
+class RefSectionRemover_old (BaseRefSectionRemover):
+    '''
+    Class to predict/remove Reference sections from (extracted) text strings
+    Simple approach, look for last occurrance of key terms that could be
+       close to the reference section start.
+    '''
+    refKeyTerms = [ "References",	# terms that can begin a ref section
+		    "Reference",
+		    "Literature Cited",
+		    "Acknowledgements",
+		    "Acknowledgments",
+		    "Conflicts of Interest",
+		    "Conflict of Interest",
+		    ]
+
+    def __init__(self,
+		 keyTerms=refKeyTerms,
+		 maxFraction=0.4	# max fraction of whole doc that the
+		 			#  ref section is allowed to be
+		):
+	self.keyTerms = keyTerms
+	self.maxFraction = maxFraction
+	self.regexString = self.buildRegexString(self.keyTerms)
+	self.refRegex = re.compile(self.regexString)
+    # -----------------------
+
+    def predictRefSection(self, text):
+	"""
+	Predict ref section.
+	Return the text found that we presume starts of the reference
+	    section and the position of it in full text.
+	"""
+	textLength = len(text)
+	matches = [ x for x in self.refRegex.finditer(text)]
+
+	if len(matches) == 0:
+	    splitKeyWord = "No match"
+	    refStart = textLength
+	else:
+	    m = matches[-1]
+	    splitKeyWord = m.group(1)	# last matched term
+	    refStart = m.start(1)	# position at end of that term
+
+	    if not self.isReasonableRefStart(refStart, textLength):
+		splitKeyWord = "Ref Prediction > %4.2f" % self.maxFraction
+		refStart = textLength
+
+	return splitKeyWord, refStart
+    # ----------------------------------
+	reason = "Ref Prediction > %4.2f" % self.maxFraction
+
+#------------------ end Class RefSectionRemover1
 
 def spacesAndCaseInsensitiveRegex(s):
     # for given string, return regex pattern string that matches the chars
@@ -102,9 +253,21 @@ def spacesAndCaseInsensitiveRegex(s):
 # -----------------------
 
 if __name__ == "__main__":
-    rm = RefSectionRemover(maxFraction=0.5, keyTerms=['Refs'])
+    print
+    print "Initial approach"
+    rm = RefSectionRemover(maxFraction=0.7, keyTerms=['Refs'])
     print "Regex: %s" % rm.getRegexString()
-    shortDoc = "this is the body ........... \nR EFS\n literature section"
-    print "doc: %s" % shortDoc
-    print "body: %s" % rm.getBody(shortDoc)
-    print "refs: %s" % rm.getRefSection(shortDoc)
+    shortDoc = "Here is the body ........... \nR ef S\n Here is literature section"
+    print "whole doc: '%s'" % shortDoc
+    print "body: '%s'" % rm.getBody(shortDoc)
+    print "refs: '%s'" % rm.getRefSection(shortDoc)
+
+    print
+    print "New approach"
+    rm = RefSectionRemover_new(maxFraction=0.7,)
+    print "Regex: %s" % rm.getRegexString()
+    shortDoc = "Here is the body ........... \nR eferences\n Here is literature section"
+    print "whole doc: '%s'" % shortDoc
+    print rm.predictRefSection(shortDoc)
+    print "body: '%s'" % rm.getBody(shortDoc)
+    print "refs: '%s'" % rm.getRefSection(shortDoc)
