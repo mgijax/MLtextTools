36cdc473f6c3455a08099ccd9008b9cb4c8be3ca
Jim Kadin <james.kadin@jax.org>
2018-07-17 15:46:41 -0400

    improved refs prediction algorithm. See Readme.

--- RefSection/refSectionLib.py
+++ RefSection/refSectionLib.py
@@ -93,6 +93,126 @@ class RefSectionRemover (BaseRefSectionRemover):
     Looks 1st for primary key terms, and if found uses their earliest location.
     If no primary key terms are found, then look for other potential terms, 
     use earliest location.
+    New:  move key term "Reference" into the secondary term list
+          Add minimum distance for key word from the end of the doc
+    '''
+    # Dict of key terms and whether they are a primary term
+    keyTermDict = {
+		"References":            {"isPrimary":True},
+		"Literature Cited":      {"isPrimary":True},
+		"Reference":             {"isPrimary":False},
+		"Acknowledgements":      {"isPrimary":False},
+		"Acknowledgments":       {"isPrimary":False},
+		"Conflicts of Interest": {"isPrimary":False},
+		"Conflict of Interest":  {"isPrimary":False},
+		}
+    # dict of squashed key terms (no spaces, lower case)
+    squashedKeyTerms = \
+	dict( [ [x.replace(" ","").lower(), x] for x in keyTermDict.keys() ] )
+
+    def __init__(self,
+		 minFraction=0.05,	# min fraction predicted for ref section
+		 maxFraction=0.4,	# max fraction of whole doc that the
+		 			#  ref section is allowed to be
+		):
+	self.keyTerms = self.keyTermDict.keys()
+	self.minFraction = minFraction
+	self.maxFraction = maxFraction
+	self.regexString = self.buildRegexString(self.keyTerms)
+	self.refRegex = re.compile(self.regexString)
+    # -----------------------
+
+    def predictRefSection(self, text):
+	"""
+	Predict ref section.
+	Return the text found that we presume starts of the reference
+	    section and the position of it in full text.
+	"""
+	textLength = len(text)
+
+	# match the reg exp and get list of re.match objects (in order matched)
+	reMatches = [ x for x in self.refRegex.finditer(text)]
+
+	if len(reMatches) == 0:
+	    return "No match", textLength
+
+	primary, secondary = self.organizeMatches(reMatches)
+
+	primary.reverse()
+	secondary.reverse()
+	for m in primary:
+	    if self.isReasonableRefStart(m['termStart'], textLength):
+		return m['matchedText'], m['termStart']
+
+	for m in secondary:
+	    if self.isReasonableRefStart(m['termStart'], textLength):
+		return m['matchedText'], m['termStart']
+
+	# no match with reasonable location found
+	reason = "No match in loc constraints" 
+	return reason, textLength
+    # ----------------------------------
+
+    def isReasonableRefStart(self, refStart, totalTextLength):
+	"""
+	Is the predicted reference start location ok?
+	(or is it too early in the document so too much of the doc would be
+	 thrown away?)
+	Assumes subclass has self.maxFraction
+	"""
+	refLength = totalTextLength - refStart
+	percentRefSection = float(refLength)/totalTextLength
+	return percentRefSection <= self.maxFraction \
+		and percentRefSection >=self.minFraction
+    # ----------------------------------
+
+    def organizeMatches(self, reMatches):
+	"""
+	Return two lists: "match descriptions" to primary terms and to
+	    secondary terms.
+	Each "match description" is
+	    {'termStart': n, 'matchedText': str, 'keyTerm': str}
+	"""
+	primary = []
+	secondary = []
+
+	for reM in reMatches:
+
+	    # assume there is only one group (w/ the matching text) per reMatch
+	    matchedText = reM.group(1)
+	    keyTerm = self.getMatchedKeyTerm(matchedText)
+	    matchDesc = {'matchedText': matchedText,
+			'keyTerm':     keyTerm,
+			'termStart':   reM.start(1),
+			}
+	    if self.keyTermDict[keyTerm]['isPrimary']:
+		primary.append(matchDesc)
+	    else:
+		secondary.append(matchDesc)
+
+	return primary, secondary
+    # ----------------------------------
+
+    def getMatchedKeyTerm(self, matchedText):
+	"""
+	Convert matchedText to an actual key term.
+	(remove spaces, lower case)
+	This effectively "undoes" the weirdness we do to create the regular
+	expression for matching the key terms
+	"""
+	cleansedTerm = matchedText.replace(" ","").lower()
+	return self.squashedKeyTerms[cleansedTerm]
+    # ----------------------------------
+
+#------------------ end Class RefSectionRemover
+
+class RefSectionRemover_Jul12 (BaseRefSectionRemover):
+    '''
+    Class to predict/remove Reference sections from (extracted) text strings.
+    Approach:
+    Looks 1st for primary key terms, and if found uses their earliest location.
+    If no primary key terms are found, then look for other potential terms, 
+    use earliest location.
     '''
     # Dict of key terms and whether they are a primary term
     keyTermDict = {
@@ -187,7 +307,7 @@ class RefSectionRemover (BaseRefSectionRemover):
 
 #------------------ end Class RefSectionRemover_new
 
-class RefSectionRemover_old (BaseRefSectionRemover):
+class RefSectionRemover_orig (BaseRefSectionRemover):
     '''
     Class to predict/remove Reference sections from (extracted) text strings
     Simple approach, look for last occurrance of key terms that could be
@@ -254,17 +374,8 @@ def spacesAndCaseInsensitiveRegex(s):
 
 if __name__ == "__main__":
     print
-    print "Initial approach"
-    rm = RefSectionRemover(maxFraction=0.7, keyTerms=['Refs'])
-    print "Regex: %s" % rm.getRegexString()
-    shortDoc = "Here is the body ........... \nR ef S\n Here is literature section"
-    print "whole doc: '%s'" % shortDoc
-    print "body: '%s'" % rm.getBody(shortDoc)
-    print "refs: '%s'" % rm.getRefSection(shortDoc)
-
-    print
-    print "New approach"
-    rm = RefSectionRemover_new(maxFraction=0.7,)
+    print "Latest approach"
+    rm = RefSectionRemover(maxFraction=0.6,)
     print "Regex: %s" % rm.getRegexString()
     shortDoc = "Here is the body ........... \nR eferences\n Here is literature section"
     print "whole doc: '%s'" % shortDoc
