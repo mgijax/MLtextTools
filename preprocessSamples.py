135103bad43865282f63868d3d64095a93e8bf7e
Jim Kadin <james.kadin@jax.org>
2019-07-15 10:34:28 -0400

    preprocessSamples.py uses sampleDataLib.ClassifiedSampleSet

--- preprocessSamples.py
+++ preprocessSamples.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python2.7 
 #
-# preprocessSamples.py
-# Takes one or more files of samples and applies various transformations
-#   to the samples, and writes transformed samples to stdout
+# Takes one or more input files of samples and applies various preprocessors
+#   to the samples, and writes preprocessed samples to stdout
 #
-# SampleRecord Class in sampleDataLib.py is responsible for the sample
-#   handling and transformations and is intended to encapsulate all the 
+# Sample Class in sampleDataLib.py is responsible for the sample
+#   handling and preprocessors and is intended to encapsulate all the 
 #   sample details.
 #
-# Assumes all input files have the same column structure. see SampleRecord.
+# Assumes all input files have the same column structure. see Sample.
 #
 # Note if no preprocessor steps are specified, this will intelligently cat
 # the sample files, collapsing down to 1 header line at the start of the output
@@ -20,7 +19,6 @@ import string
 import os
 import time
 import argparse
-
 #-----------------------------------
 
 def parseCmdLine():
@@ -28,14 +26,17 @@ def parseCmdLine():
     description='Apply preprocessor steps to files of samples. Write to stdout')
 
     parser.add_argument('inputFiles', nargs=argparse.REMAINDER,
-    	help='files of samples')
+    	help='files of samples, "-" for stdin')
 
-    parser.add_argument('-p', '--preprocessor', metavar='PREPROCESSOR',
-	dest='preprocessors', action='append', required=False, default=[],
-    	help='preprocessor method name in sampleDataLib.SampleRecord, multiples are applied in order. Default is none.' )
+    parser.add_argument('--omitrejects', dest='omitRejects', action='store_true',
+	required=False, help="don't write reject samples, default is write")
 
     parser.add_argument('-q', '--quiet', dest='verbose', action='store_false',
 	required=False, help="skip helpful messages to stderr")
+
+    parser.add_argument('-p', '--preprocessor', metavar='PREPROCESSOR',
+	dest='preprocessors', action='append', required=False, default=[],
+    	help='preprocessor method name in sampleDataLib, multiples are applied in order. Default is none.' )
 
     args = parser.parse_args()
 
@@ -55,49 +56,43 @@ def main():
     import sampleDataLib
 
     verbose("Preprocessing steps: %s\n" % ' '.join(args.preprocessors)) 
-    recordSep = sampleDataLib.RECORDSEP
     counts = { 'samples':0, 'skipped':0}
-    headerLine = None		# None until we read 1st header line in 1st file
+    firstFile = True
     startTime = time.time()
 
     for fn in args.inputFiles:
 
 	verbose("Reading %s\n" % fn)
-	lines = open(fn,'r').read().split(recordSep)
-	del lines[-1]			# empty line at end after split
 
-	if not headerLine:		# print header only @ start of 1st file
-	    headerLine = lines[0]
-	    sys.stdout.write(headerLine + recordSep)
+	if fn == '-': fn = sys.stdin
 
-	del lines[0]			# delete header line 
+	# JIM: Classified vs Unclassified ? will need to deal with this
+	sampleSet = sampleDataLib.ClassifiedSampleSet()
+	sampleSet.read(fn)
 
 	# save prev sample ID for printing if we get an exception.
-	# Give us a fighting chance of finding the offending record!
+	# Gives us a fighting chance of finding the offending record
 	prevSampleName = 'very first sample'
 
-	for rcdnum, line in enumerate(lines):
+	for rcdnum, sample in enumerate(sampleSet.sampleIterator()):
+	    counts['samples'] += 1
 	    try:
-		counts['samples'] += 1
-		if counts['samples'] % 500 == 0:	# print every 500
-		    verbose("%d.." % counts['samples'] )
-
-		sr = sampleDataLib.SampleRecord(line)
 		for pp in args.preprocessors:
-		    sr = getattr(sr,pp)()	# call preprocessor method on sr
+		    sample = getattr(sample, pp)()  # preprocessor method on sample
 
-		if sr.isReject():
-		    verbose("%s, %s skipped\n" % \
-				(sr.getRejectReason(), sr.getSampleName()) )
+		if args.omitRejects and sample.isReject():
+		    verbose( "%s, %s skipped\n" % \
+				(sample.getRejectReason(), sample.getSampleName()) )
 		    counts['skipped'] += 1
-		else: sys.stdout.write(sr.getSampleAsText())
-		prevSampleName = sr.getSampleName()
+		prevSampleName = sample.getSampleName()
 	    except:
-		sys.stderr.write( \
-		"\nException in %s record %s prevID %s\n\n" % \
-				    (fn, rcdnum, prevSampleName)
-		)
+		sys.stderr.write( "\nException in %s record %s prevID %s\n\n" % \
+				    (fn, rcdnum, prevSampleName)  )
 		raise
+
+	sampleSet.write( sys.stdout, writeHeader=firstFile,
+						    omitRejects=args.omitRejects )
+	firstFile = False
 
     verbose("Samples processed: %d \t Samples skipped: %d\n" % \
 					(counts['samples'], counts['skipped']) )
@@ -105,8 +100,9 @@ def main():
 # ---------------------
 
 def verbose(text):
-    if args.verbose: sys.stderr.write(text)
-
+    if args.verbose:
+	sys.stderr.write(text)
+	sys.stderr.flush()
 # ---------------------
 if __name__ == "__main__":
     main()
