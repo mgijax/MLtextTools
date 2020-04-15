e1fe7d2013d023b45f23d9bc397d719b0be217e2
Jim Kadin <james.kadin@jax.org>
2020-04-15 16:33:08 -0400

    convert templates to python 3.7.4

--- Templates/TuningTemplate.py
+++ Templates/TuningTemplate.py
@@ -1,5 +1,6 @@
 import sys
 import textTuningLib as tl
+import sklearnHelperLib as skHelper
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.preprocessing import StandardScaler, MaxAbsScaler
@@ -7,33 +8,34 @@ from sklearn.linear_model import SGDClassifier
 #-----------------------
 args = tl.args
 randomSeeds = tl.getRandomSeeds( { 	# None means generate a random seed
-		'randForSplit'      : args.randForSplit,
-		'randForClassifier' : args.randForClassifier,
-		} )
+                'randForSplit'      : args.randForSplit,
+                'randForClassifier' : args.randForClassifier,
+                } )
 pipeline = Pipeline( [
 ('vectorizer', CountVectorizer(
 #('vectorizer', TfidfVectorizer(
-		strip_accents=None,	# if done in preprocessing
-		decode_error='strict',	# if handled in preproc
-		lowercase=False,	# if done in preprocessing
-		stop_words='english',
-		binary=True,
-		#token_pattern=r'\b([a-z_]\w+)\b', Use default for now
-		),),
+                strip_accents=None,	# if done in preprocessing
+                decode_error='strict',	# if handled in preproc
+                lowercase=False,	# if done in preprocessing
+                stop_words='english',
+                binary=True,
+                #token_pattern=r'\b([a-z_]\w+)\b', Use default for now
+                ),),
 #('scaler'    ,StandardScaler(copy=True,with_mean=False,with_std=True)),
 #('scaler'    , MaxAbsScaler(copy=True)),
 ('classifier', SGDClassifier(verbose=0, class_weight='balanced',
-		random_state=randomSeeds['randForClassifier']) ),
+                random_state=randomSeeds['randForClassifier']) ),
 ] )
 parameters={'vectorizer__ngram_range':[(1,2)],
-	'vectorizer__min_df':[10],
-	'vectorizer__max_df':[.75],
-	'classifier__alpha':[1],
-	'classifier__learning_rate':['invscaling'],
-	'classifier__eta0':[ .01],
-	'classifier__loss':[ 'hinge' ],
-	'classifier__penalty':['l2'],
-	}
-p = tl.TextPipelineTuningHelper( pipeline, parameters,
-		    randomSeeds=randomSeeds,         ).fit()
-print p.getReports()
+        'vectorizer__min_df':[10],
+        'vectorizer__max_df':[.75],
+        'classifier__alpha':[1],
+        'classifier__learning_rate':['invscaling'],
+        'classifier__eta0':[ .01],
+        'classifier__loss':[ 'hinge' ],
+        'classifier__penalty':['l2'],
+        }
+note='\n'.join(["line 1", ]) + '\n'
+p = tl.TextPipelineTuningHelper(pipeline, parameters, randomSeeds=randomSeeds,
+                                note=note,).fit()
+print(p.getReports())
